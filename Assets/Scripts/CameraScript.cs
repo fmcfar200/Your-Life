a6622df205a176b8f57bed0e0594c2bdde44f41e
fmcfar200 <fmcfar200@caledonian.ac.uk>
2017-02-06 18:27:23 +0000

    Init

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    GameObject target;
+    public bool orbitLeft = false;
+    public bool orbitRight = false;
+    public bool orbitUp = false;
+    public bool orbitDown = false;
+
+	void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+        if (target == null)
+        {
+            Debug.LogError("CAMERA CANNOT FIND PLAYER!");
+        }
+
+	}
+	
+	void FixedUpdate()
+    {
+	    if (target !=null)
+        {
+            transform.LookAt(target.transform);
+        }
+
+        if (orbitLeft)
+        {
+            transform.RotateAround(target.transform.position, Vector3.up, Time.deltaTime * 20.0f);
+        }
+        else if (orbitRight)
+        {
+            transform.RotateAround(target.transform.position, -Vector3.up, Time.deltaTime * 20.0f);
+
+        }
+
+        else if (orbitUp)
+        {
+            transform.RotateAround(target.transform.position, Vector3.right, Time.deltaTime * 20.0f);
+
+        }
+        else if (orbitDown)
+        {
+            transform.RotateAround(target.transform.position, Vector3.left, Time.deltaTime * 20.0f);
+
+        }
+    }
+
+    public void SetOrbit(string direction)
+    {
+        if (direction == "Left")
+        {
+            orbitLeft = true;
+        }
+        else if (direction == "Right")
+        {
+            orbitRight = true;
+        }
+        else if (direction == "Up")
+        {
+            orbitUp = true;
+        }
+        else if (direction == "Down")
+        {
+            orbitDown = true;
+        }
+    }
+
+    public void DisableOrbit()
+    {
+        orbitLeft = false;
+        orbitRight = false;
+        orbitUp = false;
+        orbitDown = false;
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerMovement : MonoBehaviour {
+
+    GameObject player;
+    float speed;
+    Vector3 hitPoint;
+    bool moving;
+
+    void Start()
+    {
+        player = this.gameObject;
+        speed = 2.5f;
+        moving = false;
+    }
+
+	void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.collider.tag == "Ground")
+                {
+                    hitPoint = hit.point;
+                    moving = true;
+                }
+            }
+        }
+
+
+        if (moving)
+        {
+            MoveToPoint(hitPoint);
+        }
+    }
+
+    void MoveToPoint(Vector3 hitPoint)
+    {
+        hitPoint.y = 0;
+        player.transform.position = Vector3.MoveTowards(player.transform.position,hitPoint,speed*Time.deltaTime);
+    }
+}
