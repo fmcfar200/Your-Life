78e6c25aa66ea593a788f69a50681b53059bd6c0
fmcfar200 <fmcfar200@caledonian.ac.uk>
2017-02-15 16:39:19 +0000

    Added Prompt Menu

--- Assets/ItemScript.cs
+++ Assets/ItemScript.cs
@@ -1,14 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ItemScript : MonoBehaviour {
 
     bool clicked;
-  
+    bool promptOpen;
+
+    GameObject quickPrompt;
+    Text promptTitle;
+    Text promptDesc;
+    Button yesButton;
+    Button noButton;
+
 
     void Start()
     {
         clicked = false;
+        promptOpen = false;
+
+        quickPrompt = GameObject.Find("Quick_Prompt");
+        if (quickPrompt != null)
+        {
+            promptTitle = quickPrompt.transform.FindChild("Prompt_Title_Text").GetComponent<Text>();
+            promptDesc = quickPrompt.transform.FindChild("Prompt_Desc_Text").GetComponent<Text>();
+            yesButton = quickPrompt.transform.FindChild("Yes_Button").GetComponent<Button>();
+            noButton = quickPrompt.transform.FindChild("No_Button").GetComponent<Button>();
+        }
+        else
+        {
+            Debug.LogError("QP not found !!");
+
+        }
     }
 
     void Update()
@@ -18,14 +41,15 @@ public class ItemScript : MonoBehaviour {
             this.GetComponent<BoxCollider>().enabled = false;
             if (this.gameObject.tag == "Mission Item")
             {
-                MissionManagerScript missionManager;
-                GameObject manager = GameObject.Find("MenuManager");
-                if (manager != null)
-                {
-                    missionManager = manager.GetComponent<MissionManagerScript>();
+               
                     if (gameObject.name == "Newspaper_Item")
                     {
-                        missionManager.GetNewJob();
+                        promptOpen = true;
+                        promptTitle.text = "Paper Boy/Girl Wanted!";
+                        promptDesc.text = "The local newspaper is looking for a new paper boy/girl. Would you like to apply?";
+
+                        yesButton.onClick.AddListener(()=>YesClick());
+                        noButton.onClick.AddListener(() => NoClick());
                     }
                 }
                 else
@@ -33,7 +57,20 @@ public class ItemScript : MonoBehaviour {
                     Debug.LogError("MANAGER NOT FOUND!");
                 }
                 clicked = false;
-            }
+            
+
+        }
+
+        if (promptOpen == true)
+        {
+            quickPrompt.SetActive(true);
+            Time.timeScale = 0;
+            
+        }
+        else
+        {
+            Time.timeScale = 1;
+            quickPrompt.SetActive(false);
 
         }
     }
@@ -42,5 +79,30 @@ public class ItemScript : MonoBehaviour {
     {
         Debug.Log("Clicked on " + this.name);
         clicked = true;
+    }
+
+    void YesClick()
+    {
+        MissionManagerScript missionManager;
+        GameObject manager = GameObject.Find("MenuManager");
+        if (manager != null)
+        {
+            missionManager = manager.GetComponent<MissionManagerScript>();
+            if (gameObject.name == "Newspaper_Item")
+            {
+                missionManager.GetNewJob();
+
+                promptOpen = false;
+                quickPrompt.SetActive(false);
+                promptDesc.text = null;
+                promptTitle.text = null;
+
+            }
+        }
+    }
+
+    void NoClick()
+    {
+        promptOpen = false;
     }
 }
