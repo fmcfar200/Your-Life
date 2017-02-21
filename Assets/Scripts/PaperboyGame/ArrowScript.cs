58390f09a23575cd1ea5f46a02cf367b11e7e2f2
fmcfar200 <fmcfar200@caledonian.ac.uk>
2017-02-21 21:08:46 +0000

    Change to paperboy

---
+++ Assets/Scripts/PaperboyGame/ArrowScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowScript : MonoBehaviour {
+
+    void Update()
+    {
+        if(this.transform.position.y <= -5)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
