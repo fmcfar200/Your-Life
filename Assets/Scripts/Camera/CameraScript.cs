2e827ea11cdceb743f17ae175f5df7a4c747b075
fmcfar200 <fmcfar200@caledonian.ac.uk>
2017-02-26 22:06:27 +0000

    Bug fixes

--- Assets/Scripts/Camera/CameraScript.cs
+++ Assets/Scripts/Camera/CameraScript.cs
@@ -15,14 +15,14 @@ public class CameraScript : MonoBehaviour {
         }
 	}
 
-    void FixedUpdate()
+    void Update()
     {
         transform.LookAt(target.transform);
 
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
             Vector2 touchDPos = Input.GetTouch(0).deltaPosition;
-            transform.Translate(-touchDPos.x * 1.0f * Time.deltaTime, -touchDPos.y * 1.0f*Time.deltaTime, 0);
+            transform.Translate(-touchDPos.x * 3.0f * Time.deltaTime, -touchDPos.y * 3.0f*Time.deltaTime, 0);
         }
 
         Mathf.Clamp(transform.position.z, -6.0f, -9.0f);
