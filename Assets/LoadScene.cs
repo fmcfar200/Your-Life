42d8ca6c4f87910e1f00ac44fb7782255d69f5d6
fmcfar200 <fmcfar200@caledonian.ac.uk>
2017-02-27 15:09:53 +0000

    Started loading scene

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour {
+
+    public GameObject progBar;
+    float fillAmount;
+    AsyncOperation async;
+    void Start()
+    {
+        async = SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+        async.allowSceneActivation = false;
+
+    }
+
+    void Update()
+    {
+        
+            progBar.GetComponent<Image>().fillAmount = async.progress;
+       
+    }
+
+    
+}
