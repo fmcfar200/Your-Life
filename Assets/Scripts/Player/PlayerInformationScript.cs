d4741ca8c77c5d466c7520c2d01d4a520cdd56fc
fmcfar200 <fmcfar200@caledonian.ac.uk>
2017-02-28 21:07:35 +0000

    Added SHANARRI Panel

--- Assets/Scripts/Player/PlayerInformationScript.cs
+++ Assets/Scripts/Player/PlayerInformationScript.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class PlayerInformationScript : MonoBehaviour {
 
@@ -8,14 +9,19 @@ public class PlayerInformationScript : MonoBehaviour {
     public string playerName;
     public int score;
     public float overallWellbeing;
-
+    public List<int> wbStats = new List<int>();
+    
     [Header("UI Objects")]
     public Text nameText;
     public Text scoreText;
     public Text wellbeingText;
+    public List<Image> wbBars = new List<Image>();
+    public GameObject wellbeingPanel;
 
     GameObject controller;
     GameControllerScript gameController;
+
+    bool panelOpen = false;
 
     void Awake()
     {
@@ -28,6 +34,8 @@ public class PlayerInformationScript : MonoBehaviour {
             overallWellbeing = gameController.overallWellbeing;
 
         }
+
+       
     }
 
     void Update()
@@ -39,5 +47,32 @@ public class PlayerInformationScript : MonoBehaviour {
         nameText.text = gameController.playerName;
         scoreText.text = "Score: " + gameController.playerScore.ToString();
         wellbeingText.text = "Wellbeing: " + gameController.overallWellbeing.ToString("F1");
+
+        for (int i = 0; i < wbStats.Count; i++)
+        {
+            wbBars[i].fillAmount = (float)wbStats[i] / 10;
+        }
+
+        switch(panelOpen)
+        {
+            case true:
+                wellbeingPanel.SetActive(true);
+                break;
+            case false:
+                wellbeingPanel.SetActive(false);
+                break;
+        }
+    }
+
+    public void OpenWBPanel()
+    {
+        if (panelOpen)
+        {
+            panelOpen = false;
+        }
+        else
+        {
+            panelOpen = true;
+        }
     }
 }
